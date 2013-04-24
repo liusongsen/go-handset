0b0468b2774959ae007bc0c3249456581ff8bad9
liusongsen <liusongsen@liusongsen.(none)>
2013-04-25 00:27:57 +0800

    修复文件不同步

--- example/handset.go
+++ example/handset.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"github.com/liusongsen/go-handset"
+	"github.com/liusongsen/go-handset/lib"
 )
 
 func main() {
 
 	result := handset.Even(3)
 	fmt.Printf("%v\n", result)
+	fmt.Printf("%v\n", lib.Fetch("http://www.atido.com"))
 }
--- lib/spider.go
+++ lib/spider.go
@@ -1,22 +1,12 @@
 package lib
 
-//spider object 
-type Spider struct {
-	host  string
-	port  int
-	refer string
-}
-
 //use http get methode get content
-func (s *Spider) Fetch(url string) (content string) {
+func Fetch(url string) (content string) {
 
-	content = url + "content:sdfsdfsfdsdfsdf method:GET"
-	return
-}
-
-//use http post method submit data 
-func (s *Spider) Submit(url string, data interface{}) (content string) {
-
-	content = url + "content:sdfsdfsdf method:POST"
+	if url == "http://www.atido.com" {
+		content = "atido.com"
+	} else {
+		content = "atido.nets"
+	}
 	return
 }
