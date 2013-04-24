557228c3800fa8a627b103cc7aec236302da2146
liusongsen <245799856@qq.com>
2013-04-25 00:38:45 +0800

    test

--- example/handset.go
+++ example/handset.go
@@ -6,9 +6,11 @@ import (
 	"github.com/liusongsen/go-handset/lib"
 )
 
+var s lib.Spider
+
 func main() {
 
 	result := handset.Even(3)
 	fmt.Printf("%v\n", result)
-	fmt.Printf("%v\n", lib.Spider.Fetch("http://www.atido.com"))
+	fmt.Printf("%v\n", s.Fetch("http://www.atido.com"))
 }
