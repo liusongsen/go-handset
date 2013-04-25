730cfa9e9705a95446e5429a89dfb9f0cefafa00
liusongsen <liusongsen@gmail.com>
2013-04-25 09:17:22 +0800

    test

--- handset_test.go
+++ handset_test.go
@@ -7,6 +7,13 @@ import (
 func TestEven(t *testing.T) {
 	if !Even(2) {
 		t.Log("2 should be even!")
-		t.Fail()
+		t.FailNow()
+	}
+}
+
+func TestOdd(t *testing.T) {
+	if !Even(3) {
+		t.Log("3 should be even!")
+		t.FailNow()
 	}
 }
