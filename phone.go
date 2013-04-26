0906e73dc8eb96be98bff5e4870338c5d0e91d4a
liusongsen <liusongsen@gmail.com>
2013-04-26 14:53:57 +0800

    完善手机号码采集增加并行采集多站

---
+++ phone.go
@@ -0,0 +1,107 @@
+package handset
+
+import (
+	"strconv"
+)
+
+const (
+	IMOBILE = "http://www.imobile.com.cn/"
+	IP138   = "http://www.ip138.com/"
+	SHOJI   = "http://www.showji.com/"
+)
+
+//sim信息元数据
+type sim struct {
+	province string "省份"
+	city     string "城市"
+	ctype    string "卡类型"
+	source   string "来源"
+}
+
+//手机
+type phoner interface {
+	makeUrl() string
+	parse() sim
+	getKey() string
+}
+
+//imobile.com
+type imobile struct {
+	card int
+	url  string
+}
+
+//构造请求URL
+func (h imobile) makeUrl() string {
+
+	h.url = IMOBILE + strconv.Itoa(h.card)
+	return h.url
+}
+
+//正则解析匹配
+func (h imobile) parse() sim {
+
+	//fetch url
+	//parse content 
+	return sim{"广东", "广州", "中国联通3G卡", "imobile"}
+}
+
+//获取key
+func (h imobile) getKey() string {
+
+	return strconv.Itoa(h.card) + "_IMOBILE"
+}
+
+//ip138.com
+type ip138 struct {
+	card int
+	url  string
+}
+
+//构造请求URL
+func (h ip138) makeUrl() string {
+
+	h.url = IP138 + strconv.Itoa(h.card)
+	return h.url
+}
+
+//正则解析匹配
+func (h ip138) parse() sim {
+
+	//fetch url
+	//parse content 
+	return sim{"广东", "广州", "中国联通3G卡", "ip138"}
+}
+
+//获取key
+func (h ip138) getKey() string {
+
+	return strconv.Itoa(h.card) + "_IP138"
+}
+
+//showji.com
+type shouji struct {
+	card int
+	url  string
+}
+
+//构造请求URL
+func (h shouji) makeUrl() string {
+
+	h.url = SHOJI + strconv.Itoa(h.card)
+	return h.url
+}
+
+//正则解析匹配
+func (h shouji) parse() sim {
+
+	//fetch url
+	//parse content 
+	return sim{"广东", "广州", "中国联通3G卡", "shouji"}
+}
+
+//获取key
+func (h shouji) getKey() string {
+
+	return strconv.Itoa(h.card) + "_SHOUJI"
+}
